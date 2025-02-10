111f297069232229480ae031d492290806a8164a
drnomader <drnomader@gmail.com>
2025-02-11 01:04:46 +0900

    Initial commit

---
+++ src/main/java/tdd/choibeomgyun_tdd/ChoibeomgyunTddApplication.java
@@ -0,0 +1,13 @@
+package tdd.choibeomgyun_tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoibeomgyunTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoibeomgyunTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tdd/choibeomgyun_tdd/ChoibeomgyunTddApplicationTests.java
@@ -0,0 +1,13 @@
+package tdd.choibeomgyun_tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoibeomgyunTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
