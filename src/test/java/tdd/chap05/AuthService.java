332cebd39c8794deaa1b923193d4f3499eff0143
drnomader <drnomader@gmail.com>
2025-02-24 23:37:29 +0900

    Added chap05

---
+++ src/test/java/tdd/chap05/AuthService.java
@@ -0,0 +1,12 @@
+package tdd.chap05;
+
+public class AuthService {
+	public void authenticate(String id, String pw) {
+		if (id == null) {
+			throw new IllegalArgumentException("id");
+		}
+		if (pw == null) {
+			throw new IllegalArgumentException("password");
+		}
+	}
+}
