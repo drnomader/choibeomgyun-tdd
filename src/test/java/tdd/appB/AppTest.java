e8e08a83cc36ea9284db0bbe8a2ea35cdc6adcc0
drnomader <drnomader@gmail.com>
2025-03-03 23:23:55 +0900

    Added appB

---
+++ src/test/java/tdd/appB/AppTest.java
@@ -0,0 +1,46 @@
+package tdd.appB;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+	@Before
+	public void setUp() {
+
+	}
+
+	@Test
+	public void plus() {
+		assertEquals(2, 1 + 1);
+	}
+
+	@Test(expected = ArithmeticException.class)
+	public void throwEx() {
+		divide(1, 0);
+	}
+
+	@Test
+	public void throwEx2() {
+		ArithmeticException thrown = null;
+		try {
+			divide(1, 0);
+		} catch (ArithmeticException e) {
+			thrown = new ArithmeticException();
+		}
+		assertNotNull(thrown);
+		assertTrue(thrown.getMessage().contains("zero"));
+	}
+
+	private int divide(int a1, int a2) {
+		return a1 / a2;
+	}
+
+	@After
+	public void tearDown() {
+
+	}
+}
